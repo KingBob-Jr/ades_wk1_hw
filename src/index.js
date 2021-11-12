23d5254aef4ab060fde0cbec54dbc678f8b580cf
Yiping <syiping.20@ichat.sp.edu.sg>
2021-11-12 17:29:13 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const ApiRouter = require('./routers/api');
+
+// The web server
+const app = express();
+
+
+// To handle body
+app.use(express.json());
+
+// Web Server
+app.use(express.static(path.join(__dirname, 'public')));
+
+// APIs
+app.use('/api', ApiRouter);
+
+app.use(function(req, res, next) {
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.json({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
+
+// TODO: Error Handler
+
+// Listen to port 8000
+app.listen(8000, function () {
+  console.log('App listening on port 8000');
+});
