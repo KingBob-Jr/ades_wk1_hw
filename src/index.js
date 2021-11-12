c1ea25dd4ea1d05dab1aa522ffbf265462c5e7a7
Yiping <syiping.20@ichat.sp.edu.sg>
2021-11-12 17:54:11 +0800

    address

--- src/index.js
+++ src/index.js
@@ -38,5 +38,5 @@ app.use(function(req, res, next) {
 
 // Listen to port 8000
 app.listen(process.env.PORT || 3000, function onListen() {
-  var address = app.address();
+  var app = app.address();
  });
